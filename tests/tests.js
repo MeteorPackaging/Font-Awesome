9c80bb7024916813bb2efb79127e9d470d655dcf
Luca Mussi <ing.luca.mussi@gmail.com>
2015-08-09 10:20:00 +0200

    simpler code

--- tests/tests.js
+++ tests/tests.js
@@ -1,51 +1,50 @@
+/* global
+  document: false,
+  HTTP: false,
+  Tinytest: false
+*/
 'use strict';
 
-var packageName;  // there seems to be no official way of finding out the name of the very package we're testing - http://stackoverflow.com/questions/27180709/in-a-tinytest-test-file-how-do-i-get-the-name-of-the-package
+var fontAssets = [
+  'fontawesome-webfont.eot',
+  'fontawesome-webfont.svg',
+  'fontawesome-webfont.ttf',
+  'fontawesome-webfont.woff',
+  'fontawesome-webfont.woff2',
+  'FontAwesome.otf',
+];
 
 // Check that the font files are downloadable. Meteor places assets at /packages/<packageName>/.
-// Only the 'woff' and 'woff2' files will be used in modern browsers
-['eot', 'svg', 'ttf', 'woff', 'woff2'].forEach(function (font) {
-  Tinytest.addAsync(font + ' fonts are shipped', function (test, done) {
-
-    // curiously enough, the 'local-test:...' package isn't loaded into Package before calling Tinytest, so we can't do this determination outside this loop
-    if (!packageName) 
-      Object.keys(Package).forEach(function(p) {
-        if (p.search(/local-test/) > -1)
-          packageName = p.replace('local-test:', '');  // we should stop the loop, but forEach can't do that
-      });
-
-    var packagePath = packageName.replace(':', '_');  // e.g. fortawesome_fontawesome
-
-    HTTP.get(
-      '/packages/' + packagePath + '/Font-Awesome/fonts/fontawesome-webfont.' + font,
-      {
-         headers: {
-           'Cache-Control': 'no-cache'  // because Meteor has cached fonts even after they were removed from package.js (!) - https://github.com/meteor/meteor/issues/3196
-         }
-      },
-      function callback(error, result) {
+fontAssets.forEach(function (filename) {
+    Tinytest.addAsync(filename + ' font is shipped', function (test, done) {
+      var path = '/packages/fortawesome_fontawesome/upstream/fonts/' + filename;
+      HTTP.get(path, function callback(error, result) {
         if (error) {
           test.fail({message: 'Font failed to load'});
-        } else {
-          // if the file is 404, Meteor will redirect to / and return the Meteor.js boilerplate
-          test.isTrue(result.content.length > 30000, font + ' font could not be downloaded');
         }
-
+        else {
+          var errStr = filename + ' font could not be downloaded';
+          console.dir(result);
+          test.isTrue(result.content.length > 1000, errStr);
+        }
         done();
-      }
-    );
-  });
-});
+      });
+    });
+  })
+;
 
 
-// Visual check. Fonts are set by font-awesome.css in @font-face { src: url('../fonts/...') }.
-// TODO How does Meteor find those occurrences in the source and resolve them to /packages/<packageName>/fonts/... ?
+// Visual check.
+// Fonts are set by font-awesome.css in @font-face { src: url('../fonts/...') }.
+// TODO How does Meteor find those occurrences in the source and resolve them
+//      to /packages/<packageName>/fonts/... ?
 Tinytest.addAsync('Visual check', function (test, done) {
   var iconsDropZone = document.createElement('div');
   document.body.appendChild(iconsDropZone);
 
 
-  // TODO ideally we'd get src/test.html straight from this repo, but no idea how to do this from TinyTest
+  // TODO ideally we'd get src/test.html straight from this repo, but no idea
+  //      how to do this from TinyTest
   HTTP.get('http://rawgit.com/FortAwesome/Font-Awesome/master/src/test.html', function callback(error, result) {
     if (error) {
       test.fail('Error getting the icons. Do we have an Internet connection to rawgit.com?');
