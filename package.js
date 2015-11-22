6a40620006d3d886a27c4c6d8285ec817c839687
Luca Mussi <ing.luca.mussi@gmail.com>
2015-11-22 21:03:36 +0100

    use addAssets in place of addFiles for Meteor 1.2.1

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'fortawesome:fontawesome',
   summary: 'Font Awesome (official): 500+ scalable vector icons, customizable via CSS, Retina friendly',
-  version: '4.4.0',
+  version: '4.4.0_1',
   git: 'https://github.com/MeteorPackaging/Font-Awesome.git',
   documentation: 'README.md'
 });
 
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.1');
+  api.versionsFrom('1.2.1');
 
-  api.addFiles([
+  api.addAssets([
     // we bundle all font files, but the client will request only one of them via the CSS @font-face rule
     'upstream/fonts/fontawesome-webfont.eot',   // IE8 or older only understands EOT. IE9+ will read it too because it loads the first occurrence of `src`
     'upstream/fonts/fontawesome-webfont.svg',   // SVG fallback for iOS < 5 - http://caniuse.com/#feat=svg-fonts, http://stackoverflow.com/a/11002874/1269037
@@ -18,7 +18,7 @@ Package.onUse(function(api) {
     'upstream/fonts/fontawesome-webfont.woff',  // Most modern browsers
     'upstream/fonts/fontawesome-webfont.woff2', // Chrome 36+, Opera 23+; improves compression
     'upstream/fonts/FontAwesome.otf',
-  ], 'client', {isAsset: true});
+  ], 'client');
 
   api.addFiles([
     'upstream/css/font-awesome.css'
